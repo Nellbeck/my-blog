92f8b9767e8f409e87ace70a9aeddfba2bfb167e
fredrik nellbeck <Foggel@live.se>
2025-06-18 09:32:08 +0200

    added firebase_debugg to gitignore.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,7 @@
-// src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Din Firebase-konfiguration
 const firebaseConfig = {
   apiKey: "AIzaSyBeUORiUc11TbLhKD2UuBqVwNUBR3EMjkE",
   authDomain: "blogg-project-b8fca.firebaseapp.com",
@@ -13,10 +11,8 @@ const firebaseConfig = {
   appId: "1:780733801718:web:ae7657db722dcf9ea678d7",
 };
 
-// Initiera Firebase-appen
 const app = initializeApp(firebaseConfig);
 
-// Exportera auth och firestore
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
